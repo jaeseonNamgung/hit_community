7e95f68ef61a3d6e0227b2358f1a27350ced0e04
hyun <lihyun0302@naver.com>
2023-11-22 22:19:13 +0800

    hyun

--- community/src/main/java/com/hit/community/controller/BoardController.java
+++ community/src/main/java/com/hit/community/controller/BoardController.java
@@ -3,10 +3,14 @@ package com.hit.community.controller;
 import com.hit.community.dto.BoardDTO;
 import com.hit.community.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
-@RestController
+@Controller
 @RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
@@ -21,6 +25,13 @@ public class BoardController {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "home";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
     }
 
 }
--- community/src/main/java/com/hit/community/service/BoardService.java
+++ community/src/main/java/com/hit/community/service/BoardService.java
@@ -7,17 +7,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // dto <-> entity
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
-    @Autowired
+    //@Autowired
     private final BoardRepository boardRepository;
 
     public void save(BoardDTO boardDTO) {
         Board board = boardDTO.toEntity();
         boardRepository.save(board);
+    }
+
+    public List<BoardDTO> findAll() {
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for(Board board : boardList){
+            boardDTOList.add(board.toResponseDto());
+        }
+        return boardDTOList;
     }
 }
